23f22f7294b96621ca11d0b552e1dcd5b5567fc1
TheCoder3156 <37090477+TheCoder3156@users.noreply.github.com>
2019-01-25 09:22:36 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ const int devld = 0x00 // The address of the arduino
 int main()
 {
 	fd = wiringPiI2CSetup(devld);
-  
-  wiringPiI2CWriteReg8(fd, 0);
-  wiringPiI2CWriteReg8(fd, 0);
-  wiringPiI2CWriteReg8(fd, 0);
+	
+	wiringPiI2CWriteReg8(fd, 103);
+	wiringPiI2CWriteReg8(fd, 0);
+	wiringPiI2CWriteReg8(fd, 0);
+	wiringPiI2CWriteReg8(fd, 0);
   
 }
