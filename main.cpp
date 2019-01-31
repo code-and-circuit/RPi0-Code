8f9a3ec5bce4e54a5ade2f1ca82cb257dec44abd
TheCoder3156 <37090477+TheCoder3156@users.noreply.github.com>
2019-01-31 18:06:30 -0500

    double oops

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ const int devld = 0x00; // The address of the arduino
 
 int main()
 {
-	fd = wiringPiI2CSetup(devld);
+	int fd = wiringPiI2CSetup(devld);
 	
 	wiringPiI2CWriteReg8(fd, 103);
 	wiringPiI2CWriteReg8(fd, 0);
